fbfcaa87dd22c723f60b7a8eb4ddcfbf714b6f17
Victor Guala <victorguala@gmail.com>
2022-08-25 19:13:17 -0300

    Arreglando lo que estaba mal

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 /*
 Menu Maquina dispensadora de bebidas
 */
-
-let name = prompt("Como es tu nombre??");
-if (name ==""){
-    alert("Ingresa tu nombre por favor!!! 😊");
-}
-else{
-    let drink= prompt(name + " que bebida quieres? agua , cafe , te").toLowerCase();
-    if (drink == ""){
-        alert("Ingresa la bebida deseada");
+let name =prompt("Ingresa tu nombre");
+let drink 
+do{
+    console.log("1- Cafe \n 2- Te \n 3- Agua \n 0- Salir");
+    drink = Number (prompt(name + " elige una bebida"));
+    switch (drink){
+        case 0:
+            console.log ("Gracias por elegirnos");
+        break;
+        case 1:
+            console.log ("Recoge tu cafe del dispensador");
+        break;
+        case 2 :
+            console.log ("Recoge tu Te del dispensador");
+        break;
+        case 3 :
+            console.log("Recoge tu Agua del dispensador");
+        break;
+        default:
+            console.log("Elige una opcion correcta");
+        break;
     }
-    else if (drink == "agua"){
-        alert("Retira " + drink + " del dispensador")
-    }
-    else if (drink == "cafe"){
-        alert("Retira " + drink + " del dispensador")
-    }
-    else if (drink == "te"){
-        alert("Retira " + drink + " del dispensador")
-    }
-    else{
-        alert("No tenemos " + drink + " en este dispensador")
-    }
-}
-
+}while (drink !==0);
