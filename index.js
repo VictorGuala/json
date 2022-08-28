6c7a5df8716888df27dda79c91e11ce399d05959
Victor Guala <victorguala@gmail.com>
2022-08-28 11:20:05 -0300

    Simulador

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-/*
-Menu Maquina dispensadora de bebidas
-*/
-let name =prompt("Ingresa tu nombre");
-let drink 
-do{
-    console.log("1- Cafe \n 2- Te \n 3- Agua \n 0- Salir");
-    drink = Number (prompt(name + " elige una bebida"));
-    switch (drink){
-        case 0:
-            console.log ("Gracias por elegirnos");
-        break;
-        case 1:
-            console.log ("Recoge tu cafe del dispensador");
-        break;
-        case 2 :
-            console.log ("Recoge tu Te del dispensador");
-        break;
-        case 3 :
-            console.log("Recoge tu Agua del dispensador");
-        break;
-        default:
-            console.log("Elige una opcion correcta");
-        break;
+function visitor (){
+    alert ("Bienvenido a Espacio Terapeutico Getsha❤️");
+    let visitorName = prompt("Cual es tu nombre");
+    while(visitorName === "") {
+        visitorName = prompt("Cual es tu nombre");
     }
-}while (drink !==0);
+}
+function selectedItems (){
+    let items;
+    do {
+        items = parseInt(prompt("Que Servicio quiere adquirir?: \n 1- Piedras Calientes \n 2- Reiki \n 3- Hipnosis"))
+    }
+    while (items != 1 && items != 2 && items != 3)
+    switch(items){
+        case 1 :
+            return "Piedras Calientes";
+        case 2 :
+            return "Reiki";
+        case 3 :
+            return "Hipnosis";
+    }
+}
+function prices (items){
+    if (items === "Piedras Calientes"){
+        return 1500;
+    }
+    else if (items === "Reiki"){
+        return 1200;
+    }
+    else if (items === "Hipnosis"){
+        return 2500;
+    }
+}
+function payment (items, prices){
+    alert("Usted a elegido el siguiente servicio " + items + "\n Precio " + prices);
+    let pay = parseInt(prompt("Con cuanto abonas??? 💵"));
+    if (pay >= prices){
+        alert ("Gracias por su compra");
+    }
+    else {
+        alert ("Dinero insuficiente faltan "+ (prices - pay));
+    }
+}
+visitor();
+let itemsName = selectedItems();
+let itemsPrices = prices(itemsName);
+payment (itemsName,itemsPrices);
